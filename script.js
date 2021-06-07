312eabd6fec573be1a210a5d22b4d414d7bc8e8d
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-07 19:52:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-let num = 266219;
-let arr = (num).toString().split('');
+'use strict';
 
-const res = (arr.reduce((acc, rec) => acc * rec) ** 3).toString();
 
-console.log(res.substring(0,2));
+let lang = 'ru',
+    array,
+    arrRus = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    arrEng = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+if (lang === 'ru'){
+    array = arrRus;
+}else if(lang === 'en'){
+    array = arrEng;
+}
+console.log(array);
+let arr1 = [
+    ['en'],
+    ['ru']
+];
+
+
+switch(lang){
+    case  'ru':
+        console.log(arrRus); 
+        break;
+    case 'en':
+        console.log(arrEng);
+        break;
+}
+
+
+let arr3 = [];
+
+arr3[['ru']] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+arr3[['en']] = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+console.log(arr3[lang]);
+
+
+let namePerson = 'Артем',
+    result = (namePerson === 'Артем') ? 'Директор' : (namePerson === 'Максим') ? 'Преподаватель' : 'Студент';
+
+console.log(result);
