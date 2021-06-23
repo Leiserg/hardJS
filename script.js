bc97955773980fb08af5cd7727e07396b45f93ee
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-23 13:01:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-let num = 266219;
-let arr = (num).toString().split('');
+'use strict';
 
-const res = (arr.reduce((acc, rec) => acc * rec) ** 3).toString();
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+} 
+function array(){
+  let arr = [];
+  for (let i = 0; i < 7; i++){
+    arr.push(getRandomInt(10, 11111111111111111).toString());
+    if (arr[i].charAt(0) === '2' || arr[i].charAt(0) ==='4') {
+      console.log(arr[i]);
+    }
+  }
+  console.log(arr);
+}
+array();
 
-console.log(res.substring(0,2));
+///////////////////////////////////////////////////////////////////
+
+let n = 100;
+
+for (let i = 2; i <= n; i++) {
+    let primeNumber = 1;
+    if (i > 2 && i % 2 !== 0) {
+        for (let j = 3; j*j <= i ; j=j+2) {
+            if (i % j === 0) {
+                primeNumber = 0;
+                break;
+            }
+        }
+    } else if (i !== 2) {
+      primeNumber = 0;
+    } 
+	
+	if (primeNumber === 1) {
+      console.log(i + ` Делители этого числа: 1 и ${i}`);
+    }
+}
